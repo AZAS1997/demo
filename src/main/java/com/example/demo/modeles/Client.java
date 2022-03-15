24b41ac092c547e7e3cda9aea506a478b161402f
Asmae <assmaeaziaz@gmail.com>
2022-03-15 15:02:02 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/modeles/Client.java
@@ -0,0 +1,19 @@
+package com.example.demo.modeles;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@Getter
+@Setter
+@ToString
+public class Client {
+    private long id;
+    private String name;
+    public Client() {
+    }
+    public Client(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
